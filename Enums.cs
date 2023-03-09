79372394bd361687f64be020280022d9e71fe794
junyoungpark10 <junyoung.park@penta-cube.com>
2023-03-09 16:21:51 +0900

    Filter 생성(1)

---
+++ Enums.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace Jaywapp.Infrastructure
+{
+    public enum eLogicalOperator
+    {
+        AND,
+        OR,
+    }
+
+    public enum eFilteringOperator
+    {
+        [Description("=")]
+        Equal,
+        [Description("≠")]
+        NotEqual,
+        [Description("＜")]
+        LessThan,
+        [Description("≤")]
+        LessEqual,
+        [Description("＞")]
+        GreaterThan,
+        [Description("≥")]
+        GreaterEqual,
+        [Description("정규식")]
+        MatchRegex,
+        [Description("포함")]
+        Contains,
+        [Description("미포함")]
+        NotContains,
+        [Description("시작 문자열")]
+        StartsWith,
+        [Description("끝 문자열")]
+        EndsWith,
+    }
+}
---
+++ Filter/View/FilterView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Jaywapp.Infrastructure.Filter.Model;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Jaywapp.Infrastructure.Filter.View
+{
+    /// <summary>
+    /// FilterView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class FilterView : UserControl
+    {
+        #region Dependency Properties
+        public static readonly DependencyProperty FilterProperty = DependencyProperty.Register(
+            nameof(Filter), typeof(FilterGroup), typeof(FilterView), new FrameworkPropertyMetadata(null));
+        #endregion
+
+        #region Properties
+        public FilterGroup Filter
+        {
+            get => (FilterGroup)GetValue(FilterProperty);
+            set => SetValue(FilterProperty, value);
+        }
+        #endregion
+
+        #region Constructor
+        public FilterView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Functions
+        private void AddFilter(object sender, RoutedEventArgs e)
+        {
+            Filter.Children.Add(new CarNameFilter());
+            RaisePropertyChanged(nameof(Filter.Children));
+        }
+
+        private void AddFilterGroup(object sender, RoutedEventArgs e)
+        {
+            Filter.Children.Add(new CustomRuleCheckFilterGroup());
+            RaisePropertyChanged(nameof(Filter.Children));
+        }
+        #endregion
+    }
+}
